e14407749a27e61d3ef533640830d10d802b525a
Dudemanguy <random342@airmail.cc>
2024-03-16 11:26:04 -0500

    ci/build-openbsd: also build libglvnd
    Needed for newer versions of glad and the package isn't provided by
    openbsd itself.

--- ci/build-openbsd.sh
+++ ci/build-openbsd.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
 
-# libplacebo on openBSD is too old; use a subproject
+gitclone="git clone --depth=1 --recursive --shallow-submodules"
 rm -rf subprojects
 mkdir -p subprojects
-git clone https://code.videolan.org/videolan/libplacebo.git \
-    --recurse-submodules --shallow-submodules \
-    --depth=1 --recurse-submodules subprojects/libplacebo
+
+# build libglvnd for libplacebo/glad
+# not actually a subproject but just throw it in the directory
+$gitclone https://gitlab.freedesktop.org/glvnd/libglvnd subprojects/libglvnd
+meson setup subprojects/libglvnd/build
+meson install -C subprojects/libglvnd/build
+
+# libplacebo on openBSD is too old; use a subproject
+$gitclone https://code.videolan.org/videolan/libplacebo.git subprojects/libplacebo
 
 meson setup build \
     -Dlibmpv=true \
